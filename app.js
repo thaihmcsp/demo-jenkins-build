715999d615427f030e5eaae26f216b729d5c13f5
Hoan Nguyen <hoan.nguyen@setacinq.vn>
2023-12-22 18:13:52 +0700

    save

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const pool = mysql.createPool({
   // password: process.env.DB_PASSWORD,
   // database: process.env.DB_NAME,
   host: "mysql",
+  port: 3306,
   user: "root",
   password: "123456",
   database: "nodejs",
